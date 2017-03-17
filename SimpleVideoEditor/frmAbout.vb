68284ae819ea2cca251c0ae811ddd3c915858896
Neil Harbin <neil.harbin@ops-solutions.com>
2017-03-17 13:53:53 -0400

    Fixed github link not being clickable. Changed many items to icons.

--- SimpleVideoEditor/frmAbout.vb
+++ SimpleVideoEditor/frmAbout.vb
@@ -10,17 +10,27 @@
         End If
         Me.Text = String.Format("About {0}", ApplicationTitle)
         ' Initialize all of the text displayed on the About Box.
-        ' TODO: Customize the application's assembly information in the "Application" pane of the project 
-        '    properties dialog (under the "Project" menu).
         Me.LabelProductName.Text = My.Application.Info.ProductName
         Me.LabelVersion.Text = String.Format("Version {0}", My.Application.Info.Version.ToString)
         Me.LabelCopyright.Text = My.Application.Info.Copyright
         Me.LabelCompanyName.Text = My.Application.Info.CompanyName
         Me.TextBoxDescription.Text = My.Application.Info.Description
+        'Assure the link is clickable
+        If Not Me.lblGithubLink.Links.Count > 0 Then
+            Me.lblGithubLink.Links.Add(New LinkLabel.Link(0, lblGithubLink.Text.Length, "https://www.github.com/OPS-Solutions/SimpleVideoEditor"))
+        Else
+            Me.lblGithubLink.Links(0) = (New LinkLabel.Link(0, lblGithubLink.Text.Length, "https://www.github.com/OPS-Solutions/SimpleVideoEditor"))
+        End If
     End Sub
 
     Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
         Me.Close()
     End Sub
 
+    ''' <summary>
+    ''' Starts a process to open the link
+    ''' </summary>
+    Private Sub lblGithubLink_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lblGithubLink.LinkClicked
+        System.Diagnostics.Process.Start(e.Link.LinkData.ToString())
+    End Sub
 End Class
