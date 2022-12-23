eba3461a49ea5086444322fa06328e247f533009
Neil Harbin <neilharbin0@gmail.com>
2022-12-23 15:40:53 -0500

    -Improvement- Changed pixel offset of preview to better align low resolution thumbnail images to the edges of the view

--- SimpleVideoEditor/Controls/PictureBoxPlus.vb
+++ SimpleVideoEditor/Controls/PictureBoxPlus.vb
@@ -6,6 +6,7 @@ Public Class PictureBoxPlus
 
     Protected Overrides Sub OnPaint(pe As PaintEventArgs)
         pe.Graphics.InterpolationMode = Me.InterpolationMode
+        pe.Graphics.PixelOffsetMode = PixelOffsetMode.Half
         MyBase.OnPaint(pe)
     End Sub
 End Class
